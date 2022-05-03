0dcee77b38f7a68673beca3c392f453cd63f795a
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2022-05-03 19:13:00 -0300

    Add files via upload

---
+++ project_c9_pro_new-main/sketch.js
@@ -0,0 +1,48 @@
+
+function setup() {
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
+
+}
+
+function draw() 
+{
+
+  // escreva o código para alterar a cor de fundo 
+  // para vermelho quando a seta para direita (RIGHT_ARROW) for pressionada
+  
+  
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    box.shapeColor = "green"
+    box.rotation += -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+    box.shapeColor= "blue"
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+    box.shapeColor= "red"
+  }
+
+  if (keyIsDown(RIGHT_ARROW)){
+    background("red");
+    box.shapeColor = "yellow"
+    box.rotation += 5;
+  }
+
+
+  
+  drawSprites();
+}
+
